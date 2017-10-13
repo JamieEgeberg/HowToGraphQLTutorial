54e4ffa04829c794b84aa0125f40a41f6d9e8fdd
JamieEgeberg <jamie_egeberg@hotmail.com>
2017-10-13 13:25:15 +0200

    Fake (Hacker)news!

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import './styles/index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { GC_AUTH_TOKEN } from './constants'
+import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
+
+// 1
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+// 2
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj8mpquwe0gsm0176pq0djy7q'
+})
+
+const wsClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj8mpquwe0gsm0176pq0djy7q', {
+  reconnect: true,
+  connectionParams: {
+     authToken: localStorage.getItem(GC_AUTH_TOKEN),
+  }
+})
+
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+  networkInterface,
+  wsClient
+)
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
+
+const client = new ApolloClient({
+  networkInterface: networkInterfaceWithSubscriptions
+})
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+)
+registerServiceWorker()
